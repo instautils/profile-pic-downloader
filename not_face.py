0c9a3be2b12b87711be637f77884a3f7f1269b96
Ahmadreza Zibaei <zibaeiahmadreza@gmail.com>
2019-04-24 18:15:27 +0430

    remove image if has error

--- not_face.py
+++ not_face.py
@@ -1,22 +1,30 @@
 import cv2
 import dlib
 import os
+import multiprocessing
 from tqdm import tqdm
 from glob import glob
 
 detector = dlib.get_frontal_face_detector()
 
-counter = 0
-for image_file in tqdm(glob('images/*.jpg')):
+
+def check_image(image_file):
     try:
         image = cv2.imread(image_file)
     except BaseException:
-        continue
+        os.remove(image_file)
+        return 1
 
     image = cv2.resize(image, (256, 256))
     dets, _, _ = detector.run(image, 1, -1)
     if len(dets) == 0:
-        counter += 1
         os.remove(image_file)
+        return 1
+    return 0
 
-print "{} files removed !".format(counter)
+
+pool = multiprocessing.Pool(processes=8)
+results = [pool.apply(check_image, args=(image_file,))
+           for image_file in glob('images/*.jpg')]
+
+print "{} files removed !".format(sum(results))
