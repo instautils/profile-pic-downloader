b5bf59fdee2f01940db31a6fbf65e039ae075e41
Ahmadreza Zibaei <zibaeiahmadreza@gmail.com>
2019-04-26 17:05:23 +0430

    GenderDetector added

--- gender_detector.py
+++ gender_detector.py
@@ -3,69 +3,69 @@ import dlib
 import os
 import pickle
 import sys
+import multiprocessing
 from tqdm import tqdm
 from glob import glob
 from shutil import copyfile
 
-target_directory = 'labels'
 
-detector = dlib.get_frontal_face_detector()
-predictor = dlib.shape_predictor(
-    os.path.join("models", "shape_predictor_68_face_landmarks.dat")
-)
-recognizer = dlib.face_recognition_model_v1(
-    os.path.join("models", "dlib_face_recognition_resnet_model_v1.dat")
-)
-classifier = pickle.load(open(os.path.join('models', 'gender.pickle'), 'r'))
+class GenderDetector:
+    def __init__(self):
+        if not os.path.exists('models'):
+            raise Exception('models directory does not exist')
 
+        self.detector = dlib.get_frontal_face_detector()
+        self.predictor = dlib.shape_predictor(
+            os.path.join("models", "shape_predictor_68_face_landmarks.dat")
+        )
+        self.recognizer = dlib.face_recognition_model_v1(
+            os.path.join("models", "dlib_face_recognition_resnet_model_v1.dat")
+        )
+        self.classifier = pickle.load(
+            open(os.path.join('models', 'gender.pickle'), 'r'))
 
-def predict_gender(encoding):
-    result = classifier(dlib.vector(encoding))
-    if result > 0.4:
-        return "male"
-    if result < -0.4:
-        return "female"
-    return "unknown"
+    def face_size(self, rect):
+        return rect.width() * rect.height()
 
+    def predict_gender(self, encoding, thresh=0.4):
+        result = self.classifier(dlib.vector(encoding))
+        if result > thresh:
+            return "male"
+        if result < -thresh:
+            return "female"
+        return "unknown"
 
-def face_descriptor(img, rect):
-    return recognizer.compute_face_descriptor(img, predictor(img, rect), 1)
+    def face_descriptor(self, img, rect):
+        return self.recognizer.compute_face_descriptor(img, self.predictor(img, rect), 1)
 
-
-def face_size(face_rect):
-    return face_rect.width() * face_rect.height()
-
-
-def detect_gender_of_image(image_file):
-    try:
+    def process(self, image_file):
         image = cv2.imread(image_file)
-    except Exception:
-        return []
+        image = cv2.resize(image, (256, 256))
+        dets, scores, _ = self.detector.run(image, 1, -1)
+        if sum(scores) < 0.1:
+            return 'unknown'
 
-    image = cv2.resize(image, (256, 256))
-    dets, scores, _ = detector.run(image, 1, -1)
-    if not dets:
-        return []
-    if sum(scores) < 0.1:
-        return []
-
-    faces = list(dets)
-    faces.sort(cmp=lambda x, y: face_size(y) - face_size(x))
-    face = faces[0]
-    description = face_descriptor(image, face)
-    return predict_gender(description)
+        faces = list(dets)
+        faces.sort(cmp=lambda x, y: self.face_size(y) - self.face_size(x))
+        face = faces[0]
+        description = self.face_descriptor(image, face)
+        return self.predict_gender(description)
 
 
-def do_process(image_file):
-    target_file = os.path.basename(image_file)
-    gender = detect_gender_of_image(image_file)
-    if gender == 'unknown':
+def valid_image(image_file):
+    with open(image_file, 'rb') as handler:
+        string = handler.read(1)
+        return len(string) != 0
+
+
+def do_process(detector, image_file, target_directory='labels'):
+    if not valid_image(image_file):
         return
-
+        
     path = os.path.join(
         target_directory,
-        gender,
-        target_file,
+        detector.process(image_file),
+        os.path.basename(image_file),
     )
     if os.path.exists(path):
         return
@@ -73,5 +73,9 @@ def do_process(image_file):
     copyfile(image_file, path)
 
 
-for image_file in glob('images/*.jpg'):
-    do_process(image_file)
+if __name__ == "__main__":
+    gender_detector = GenderDetector()
+
+    pool = multiprocessing.Pool(processes=4)
+    for image_file in glob('images/*.jpg'):
+        pool.apply(do_process, args=(gender_detector, image_file))
